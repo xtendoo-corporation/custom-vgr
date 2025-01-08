3f196e5817831e8ea9a8991270193a4c9f2472b5
Gokac9 <theabraham9@gmail.com>
2025-01-08 10:49:59 +0100

    fix error with margin

--- custom_intermediary_margin/models/sale_order_line.py
+++ custom_intermediary_margin/models/sale_order_line.py
@@ -45,16 +45,14 @@ class SaleOrderLine(models.Model):
         for record in self:
             if record.purchase_price:
                 if record.profit_percentage > 0:
+                    print("*" * 100)
+                    print("Profit percentage: ",record.profit_percentage)
                     record.net_margin = record.purchase_price * (record.profit_percentage / 100)
                     record.unit_price_without_margin_intermediary = record.purchase_price + record.net_margin
                     if record.intermediary_percentage > 0:
                         print("*"*100)
                         print(record.intermediary_price)
                         record.intermediary_price = record.unit_price_without_margin_intermediary * (record.intermediary_percentage / 100)
-                        # nueva linea debajo
-                        record.margin = record.price_subtotal - (record.purchase_price * record.product_uom_qty)
-                        record.margin = record.margin - record.intermediary_price
-                        record.margin_percent = record.price_subtotal and record.margin / record.price_subtotal
                         print(record.intermediary_price)
                         record.price_unit = record.unit_price_without_margin_intermediary + record.intermediary_price
                     if record.intermediary_percentage == 0:
@@ -64,10 +62,6 @@ class SaleOrderLine(models.Model):
                     record.net_margin = record.price_unit - record.purchase_price
                     if record.intermediary_percentage > 0:
                         record.intermediary_price = record.price_unit * (record.intermediary_percentage / 100)
-                        # nueva linea debajo
-                        record.margin = record.price_subtotal - (record.purchase_price * record.product_uom_qty)
-                        record.margin = record.margin - record.intermediary_price
-                        record.margin_percent = record.price_subtotal and record.margin / record.price_subtotal
                         record.unit_price_without_margin_intermediary = record.price_unit - record.intermediary_price
                     if record.intermediary_percentage == 0:
                         record.unit_price_without_margin_intermediary = record.price_unit
@@ -76,10 +70,6 @@ class SaleOrderLine(models.Model):
                 record.net_margin = record.price_unit
                 if record.intermediary_percentage > 0:
                     record.intermediary_price = record.price_unit * (record.intermediary_percentage / 100)
-                    # nueva linea debajo
-                    record.margin = record.price_subtotal - (record.purchase_price * record.product_uom_qty)
-                    record.margin = record.margin - record.intermediary_price
-                    record.margin_percent = record.price_subtotal and record.margin / record.price_subtotal
                     record.unit_price_without_margin_intermediary = record.price_unit - record.intermediary_price
                 if record.intermediary_percentage == 0:
                     record.unit_price_without_margin_intermediary = record.price_unit
@@ -94,3 +84,9 @@ class SaleOrderLine(models.Model):
             'purchase_price': self.purchase_price,
         })
         return res
+
+    @api.depends('price_subtotal', 'product_uom_qty', 'purchase_price', 'intermediary_price')
+    def _compute_margin(self):
+        for line in self:
+            line.margin = (line.price_subtotal - (line.purchase_price * line.product_uom_qty)) - line.intermediary_price
+            line.margin_percent = line.price_subtotal and line.margin / line.price_subtotal
