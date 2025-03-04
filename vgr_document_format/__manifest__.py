e7fbe7b5fc1f5e086abed359c8681083a8ad1763
Gokac9 <theabraham9@gmail.com>
2025-03-04 14:39:40 +0100

    add module vgr_document_format

---
+++ vgr_document_format/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "VGR Document Format",
+    "summary": """VGR Document Format""",
+    "version": "17.0.1.0.0",
+    "description": """VGR Document Format""",
+    "company": "Xtendoo",
+    "author": "Abraham",
+    "website": "http://www.xtendoo.es",
+    "category": "Website",
+    "depends": [
+    ],
+    "data": [
+        'views/report_saleorder_document.xml',
+        'views/report_saleorder_document_without_value.xml',
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+    'application': True,
+}
