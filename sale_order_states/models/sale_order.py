b8e869112951fd29becf09ccc51802fae6f10c23
Gokac9 <theabraham9@gmail.com>
2025-03-10 08:45:04 +0100

    sync fix states changes

--- sale_order_states/models/sale_order.py
+++ sale_order_states/models/sale_order.py
@@ -1,4 +1,6 @@
 from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+import logging
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
@@ -10,16 +12,12 @@ class SaleOrder(models.Model):
 
     @api.model
     def _get_vgr_state_selection(self):
-        # Recuperamos todos los estados ordenados por la secuencia
         states = self.env['sale.order.state'].search([
             ('company_id', '=', self.env.company.id),
             '|',
             ('visible_presupuesto', '=', True),
             ('visible_pedido', '=', True)
         ], order='sequence')
-        # Imprimimos los estados para verlos
-        print("Estados recuperados selection:", [state.name for state in states])
-        # Devolvemos los estados ordenados para el campo de selección
         return [(state.name, state.name) for state in states]
 
     vgr_state_selection = fields.Selection(
@@ -27,6 +25,7 @@ class SaleOrder(models.Model):
         string='Custom VGR States (Selection)',
         store=True,
         group_expand = "_read_group_vgr_state_id",
+        company_dependent=True,
         domain="[('company_id', '=', user.company_id.id),'|', ('visible_presupuesto', '=', True), ('visible_pedido', '=', True)]"
     )
 
@@ -34,6 +33,7 @@ class SaleOrder(models.Model):
         selection='_get_vgr_state_presupuesto',
         string='VGR State Presupuesto',
         store=True,
+        company_dependent=True,
         group_expand="_read_group_vgr_state_id"
     )
 
@@ -41,21 +41,21 @@ class SaleOrder(models.Model):
         selection='_get_vgr_state_pedido',
         string='VGR State Pedido',
         store=True,
+        company_dependent=True,
         group_expand="_read_group_vgr_state_id"
     )
 
-    # @api.onchange('company_id')
-    # def _onchange_company_id(self):
-    #     if self.company_id:
-    #         self.vgr_state_presupuesto = False
-    #         self.vgr_state_pedido = False
+    @api.onchange('company_id')
+    def _onchange_company_id(self):
+        if self.company_id:
+            self.vgr_state_presupuesto = False
+            self.vgr_state_pedido = False
 
     def _get_vgr_state_presupuesto(self):
             states = self.env['sale.order.state'].search([
                 ('company_id', '=', self.env.company.id),
                 ('visible_presupuesto', '=', True)
             ], order='sequence')
-            print("Estados recuperados presupuesto:", [state.name for state in states])
             return [(state.name, state.name) for state in states]
 
     def _get_vgr_state_pedido(self):
@@ -63,7 +63,6 @@ class SaleOrder(models.Model):
                 ('company_id', '=', self.env.company.id),
                 ('visible_pedido', '=', True)
             ], order='sequence')
-            print("Estados recuperados pedido:", [state.name for state in states])
             return [(state.name, state.name) for state in states]
 
     @api.model
@@ -82,4 +81,3 @@ class SaleOrder(models.Model):
         for order in self:
             state = self.env['sale.order.state'].search([('name', '=', order.vgr_state_selection)], limit=1)
             order.vgr_alert_type = state.alert_type if state else 'normal'
-
