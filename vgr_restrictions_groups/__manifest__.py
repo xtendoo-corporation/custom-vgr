2eae6fb14d98093790b14898423d5958ee319a3a
Gokac9 <theabraham9@gmail.com>
2025-03-19 14:54:50 +0100

    edit report_saleorder_document

---
+++ vgr_restrictions_groups/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'VGR Restricciones de Presupuestos',
+    'version': '17.0.1.0.0',
+    'category': 'Sales, stock, account',
+    'author': 'Abraham (Xtendoo)',
+    'depends': ['sale', 'stock', 'account'],
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': False,
+}
