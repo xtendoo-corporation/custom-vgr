a1bb123b2ebc8622d514354c7a7064ad73ec3f93
manuelcalerosolis <manuelcalerosolis@gmail.com>
2025-05-23 10:02:36 +0200

    revision permisions

--- vgr_restrictions_groups/__manifest__.py
+++ vgr_restrictions_groups/__manifest__.py
@@ -3,6 +3,15 @@
     'version': '17.0.1.0.0',
     'category': 'Sales, stock, account',
     'author': 'Abraham (Xtendoo)',
+    'website': 'https://xtendoo.es',
+    'summary': 'Restricciones de presupuestos para productos y variantes',
+    'description': """
+        Este módulo restringe la creación de productos y variantes de producto
+        a usuarios con permisos específicos. Si un usuario intenta crear un
+        producto o variante sin los permisos adecuados, se lanzará un error
+        de acceso.
+    """,
+    'license': 'LGPL-3',
     'depends': ['sale', 'stock', 'account'],
     'data': [
         'security/security.xml',
