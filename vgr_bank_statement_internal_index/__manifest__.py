e293adb4574408899ef2da77a2cea92553727f6a
manuelcalerosolis <manuelcalerosolis@gmail.com>
2025-07-28 13:52:26 +0200

    new module vgr

---
+++ vgr_bank_statement_internal_index/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'VGR Bank Statement Internal Index',
+    'version': '17.0.1.0.0',
+    'category': 'Accounting',
+    'summary': 'Show internal_index field in bank statement lines',
+    'depends': ['account'],
+    'data': [
+        'views/account_bank_statement_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
+
