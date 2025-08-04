1a4a95aa5664b85b5a774722c41a9beb3942f1b7
manuelcalerosolis <manuelcalerosolis@gmail.com>
2025-08-04 14:09:57 +0200

    sync

--- vgr_bank_statement_internal_index/models/account_bank_statement.py
+++ vgr_bank_statement_internal_index/models/account_bank_statement.py
@@ -7,18 +7,33 @@ class AccountBankStatement(models.Model):
     def action_recalculate_sequences(self):
         """Recalcula las secuencias de las líneas basándose en la fecha"""
         for statement in self:
-            lines = statement.line_ids.sorted('date')
-            sequence = 1
-            for line in lines:
-                line.sequence = sequence
-                sequence += 1
+            # Ordenar las líneas por fecha y luego por ID para mantener consistencia
+            lines = statement.line_ids.sorted(lambda l: (l.date, l.id))
+
+            # Asignar nuevas secuencias
+            for index, line in enumerate(lines, start=1):
+                line.sequence = index * 10  # Incrementos de 10 para permitir inserciones
+
         return True
 
     def action_recalculate_internal_index(self):
         """Fuerza el recálculo del internal_index de todas las líneas"""
         for statement in self:
-            # Forzamos el recálculo del campo computado internal_index
-            statement.line_ids._compute_simple_internal_index()
+            # Filtrar líneas que tienen ID (están guardadas en la base de datos)
+            lines_with_id = statement.line_ids.filtered(lambda l: l._origin.id)
+
+            # Forzar el recálculo del internal_index
+            for line in lines_with_id:
+                line._compute_internal_index()
+
+        return True
+
+    def action_delete_lines(self):
+        """Borra todas las líneas del extracto bancario"""
+        for statement in self:
+            # Borrar todas las líneas del extracto
+            statement.line_ids.unlink()
+
         return True
 
 
@@ -30,4 +45,3 @@ class AccountBankStatementLine(models.Model):
             st_line.internal_index = f'{st_line.date.strftime("%Y%m%d")}' \
                                       f'{st_line.sequence:0>10}' \
                                       f'{st_line._origin.id:0>10}'
-
