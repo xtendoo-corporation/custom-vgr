f5ea01ffc002d8e44eef126e83586e60f6b1f1a3
manuelcalerosolis <manuelcalerosolis@gmail.com>
2025-10-13 13:45:16 +0200

    sync

--- vgr_bank_statement_internal_index/__manifest__.py
+++ vgr_bank_statement_internal_index/__manifest__.py
@@ -3,11 +3,10 @@
     'version': '17.0.1.0.0',
     'category': 'Accounting',
     'summary': 'Show internal_index field in bank statement lines',
-    'depends': ['account'],
+    'depends': ['account', 'account_statement_base'],
     'data': [
         'views/account_bank_statement_views.xml',
     ],
     'installable': True,
     'auto_install': False,
 }
-
